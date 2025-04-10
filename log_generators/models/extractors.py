270e65625067f4733337dd0790ae6df86a79e003
Pedro Fluxa <pfluxa@Pedros-MacBook-Air.local>
2025-04-10 07:47:00 -0400

    much larger model!

--- log_generators/models/extractors.py
+++ log_generators/models/extractors.py
@@ -157,7 +157,7 @@ class BCETrainer(Trainer):
 
 if __name__ == '__main__':
 
-    base_model_name = 'bert-large-uncased-whole-word-masking'
+    base_model_name = 'google/flan-t5-large'#bert-large-uncased-whole-word-masking'
     compute_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     n_systems, label_names, label_encoder, tokenizer = dataset_to_hf_dataset(["train", "test", "validation"], [250, 100, 50])
